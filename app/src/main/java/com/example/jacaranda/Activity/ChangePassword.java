b50dbcd1989fb7de5fcd5f58a57527fa2ed6bf4a
zhaokaig <gzk747168814@gmail.com>
2023-01-04 17:19:03 +0800

    ChangePassword (ChangePassword & ChangePwdSuccess): add functionality to it and jump to Signin page after changing password

--- app/src/main/java/com/example/jacaranda/Activity/ChangePassword.java
+++ app/src/main/java/com/example/jacaranda/Activity/ChangePassword.java
@@ -1,8 +1,15 @@
 package com.example.jacaranda.Activity;
 
+import static com.example.jacaranda.Util.JsonToStringUtil.parseResponse;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -11,20 +18,46 @@ import android.text.Spannable;
 import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jacaranda.HintPage.ChangePasswordSuccessfully;
+import com.example.jacaranda.JacarandaApplication;
 import com.example.jacaranda.R;
+import com.example.jacaranda.SelectAccount;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ChangePassword extends AppCompatActivity {
+
+    private JacarandaApplication app;
+    private static final String TAG = "ChangePassword";
+    private static final String PATH_CHANGE_PWD = "/changePswd";
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        app = (JacarandaApplication)getApplication();
+        preferences = getSharedPreferences("config", Context.MODE_PRIVATE);
+
         initAll();
     }
 
@@ -250,10 +283,106 @@ public class ChangePassword extends AppCompatActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ChangePassword.this, ChangePasswordSuccessfully.class);
-                startActivity(intent);
-                finish();
+                changePassword(parseInfo());
             }
         });
+    }
+
+    private JSONObject parseInfo(){
+
+        //parse values in textbox and transfer to json
+        JSONObject json_body = new JSONObject();
+
+        try {
+            json_body.put("oldPswd", password1.getText().toString());
+            json_body.put("newPswd", password3.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return json_body;
+    }
+
+    private void changePassword(JSONObject json_body){
+        new Thread(){
+            @Override
+            public void run() {
+
+                Log.i(TAG, json_body.toString());
+                //setup RequestBody
+                final RequestBody requestBody = RequestBody.create(
+                        json_body.toString(),
+                        MediaType.get("application/json; charset=utf-8")
+                );
+
+                Request request = new Request.Builder()
+                        .url(app.getURL() + PATH_CHANGE_PWD)
+                        .addHeader("token",preferences.getString("AccessToken", null))
+                        .post(requestBody)
+                        .build();
+
+
+                new OkHttpClient()
+                        .newCall(request)
+                        .enqueue(new Callback() {
+                            @Override
+                            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                                showAlert("Failed to load data", "Error: " + e.toString());
+                            }
+
+                            @Override
+                            public void onResponse(
+                                    @NonNull Call call,
+                                    @NonNull Response response
+                            ) throws IOException {
+                                if (!response.isSuccessful()) {
+                                    showAlert(
+                                            "Failed to load page",
+                                            "Error: " + response.toString()
+                                    );
+                                } else {
+                                    try {
+                                        final JSONObject responseJson = parseResponse(response.body());
+                                        Log.i(TAG, responseJson.toString());
+
+                                        String code, message, data, accessToken, refreshToken, userID;
+                                        code = responseJson.optString("code");
+                                        message = responseJson.optString("msg");
+                                        data = responseJson.optString("data");
+                                        Log.i(TAG, data);
+
+                                        if (code.equals("200")){
+                                            Intent intent = new Intent(ChangePassword.this, ChangePasswordSuccessfully.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }else{
+                                            showToast(message);
+                                        }
+
+                                    } catch (IOException | JSONException e) {
+                                        Log.e(TAG, "Error parsing response", e);
+                                    }
+
+                                }
+                            }
+                        });
+
+            }
+        }.start();
+    }
+
+    private void showAlert(String title, @Nullable String message) {
+        runOnUiThread(() -> {
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setPositiveButton("Ok", null)
+                    .create();
+            dialog.show();
+        });
+    }
+
+    private void showToast(String message) {
+        runOnUiThread(() -> Toast.makeText(this, message, Toast.LENGTH_LONG).show());
     }
 }
--- app/src/main/java/com/example/jacaranda/HintPage/ChangePasswordSuccessfully.java
+++ app/src/main/java/com/example/jacaranda/HintPage/ChangePasswordSuccessfully.java
@@ -2,25 +2,46 @@ package com.example.jacaranda.HintPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.jacaranda.Activity.ChangePassword;
+import com.example.jacaranda.Activity.SignInActivity;
 import com.example.jacaranda.R;
 
 public class ChangePasswordSuccessfully extends AppCompatActivity {
     Button back;
 
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        preferences = getSharedPreferences("config", Context.MODE_PRIVATE);
+
         setContentView(R.layout.activity_change_password_successfully);
         back = (Button) findViewById(R.id.id_btn_changePasswordSuccessfully_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                logOut();
             }
         });
+    }
+
+    private void logOut(){
+        SharedPreferences.Editor  editor = preferences.edit();
+        editor.clear();
+        editor.commit();
+
+        Intent intent = new Intent(ChangePasswordSuccessfully.this, SignInActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
